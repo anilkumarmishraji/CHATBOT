edae52dd3839c62c38bbca06881c69669e826ef0
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-24 23:23:26 +0530

    Update userbot.py

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -3,6 +3,8 @@ from os import getenv
 from config import OWNER_ID
 from dotenv import load_dotenv
 from pyrogram import Client
+from pytgcalls import PyTgCalls
+from pytgcalls.types import Update
 import config
 
 
@@ -17,6 +19,7 @@ class Userbot(Client):
             plugins=dict(root="nexichat.idchatbot"),
         )
         
+        self.calls = PyTgCalls(self.one)
 
     async def start(self):
         print(f"Starting Id chatbot...")
@@ -28,21 +31,27 @@ class Userbot(Client):
                 await self.one.join_chat("THE_VIP_BOY_OP")
                 await self.one.join_chat("TG_FRIENDSS")
                 await self.one.join_chat("VIP_CREATORS")
-
             except:
                 pass
             self.one.id = self.one.me.id
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
-     
+
+            
+            await self.calls.start()
+
             print(f"Id-Chatbot Started as {self.one.me.first_name}")
             
+            
+            @self.calls.on_update()
+            async def on_call_update(update: Update):
+                print(f"Voice chat update received: {update}")
         
-
     async def stop(self):
-        LOGGER(__name__).info(f"Stopping Id-Chatbot...")
+        print(f"Stopping Id-Chatbot...")
         try:
             if config.STRING1:
+                await self.calls.stop() 
                 await self.one.stop()
         except:
             pass
