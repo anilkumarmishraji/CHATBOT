1e85a716e47d5864b2ce57e0aea37e5fa2520b64
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-25 10:45:45 +0530

    Update zchatbot.py

--- nexichat/mplugin/zchatbot.py
+++ nexichat/mplugin/zchatbot.py
@@ -211,9 +211,9 @@ async def typing_effect(client, message, translated_text):
         part3 = translated_text[2 * total_length // 3:]
 
         reply = await message.reply_text(part1)
-        #await asyncio.sleep(0.8)
+        await asyncio.sleep(0.01)
         await reply.edit_text(part1 + part2)
-        #await asyncio.sleep(0.8)
+        await asyncio.sleep(0.01)
         await reply.edit_text(part1 + part2 + part3)
     except Exception as e:
         print(f"Error in typing_effect: {e}")
@@ -282,7 +282,8 @@ async def chatbot_response(client: Client, message: Message):
                 else:
                     try:
                         #await message.reply_text(translated_text)
-                        await typing_effect(client, message, translated_text)
+                        asyncio.create_task(typing_effect(client, message, translated_text))
+                        await client.send_chat_action(chat_id, ChatAction.TYPING)
                     except:
                         pass
             else:
