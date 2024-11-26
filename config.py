79d68e249250d603830e5183cfca32d48eee733a
anilkumarmishraji <anilkumarmishraji80@gmail.com>
2024-11-26 09:45:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "27345751"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "2f4074e9f5a56e99fd9ba562f2314523"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
+OWNER_ID = int(getenv("OWNER_ID", "7282582221"))
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/anilkumarmishraji/CHATBOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+SUPPORT_GRP = "HINDII_CHATTING"
+UPDATE_CHNL = "RAJA_NETWORKS"
+OWNER_USERNAME = "ll_Raja_babu_ll"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
